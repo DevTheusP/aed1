a21d8d7477b45976b574c1cb33b0e8f4958b8181
DevTheusP <matheusp.estudos@gmail.com>
2025-05-04 18:02:15 -0300

    resolução do teste, precisei revisar os conteudos de ponteiros e strings

--- sem1/teste.c
+++ sem1/teste.c
@@ -2,44 +2,105 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
-    //não lembro como realocar memória caso precisar
-    char nomes[100] = "";
-    char nometemp[50]= "";
-    int opcao = 0;
-    printf("\nDigite 1 para adicionar nome, 2 para excluir, 3 para listar e 4 para sair: ");
-    scanf("%d", &opcao);
-    getchar();
-    while (opcao != 4){
-        switch (opcao)
-        {
-        case 1:
-        printf("\nDigite o nome a ser adicionado: ");
-        fgets(nometemp, 50, stdin);
-        strcat(nomes, nometemp);
-            break;
-        case 2:
-        printf("\nDigite o nome a ser removido");
-        fgets(nometemp, 50, stdin);
-        strcspn(nomes, nometemp);
-            break;
-        
-        case 3:
-            printf("\nListando todos os nomes");
-            printf("\n%s", nomes);
-            break;
-        
-        case 4:
-            printf("\nPrograma encerrado");
-            break;
-
-        default:
-
-            break;
-        };
-        printf("\nDigite 1 para adicionar nome, 2 para excluir, 3 para listar e 4 para sair: ");
-        scanf("%d", &opcao);
-        getchar();
-    };
+int main() {
+    char *nomes = NULL;  
+    int opcao;
+    char nome[50];
     
-};
+    do {
+        
+        printf("\n=== MENU ===\n");
+        printf("1. Adicionar nome\n");
+        printf("2. Remover nome\n");
+        printf("3. Listar nomes\n");
+        printf("4. Sair\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+        getchar();  // Limpar o buffer
+        
+        switch(opcao) {
+            case 1:  
+                printf("Digite o nome: ");
+                fgets(nome, 50, stdin);
+                nome[strlen(nome)-1] = '\0';  // Remover o \n
+                
+                if(nomes == NULL) {
+                    nomes = (char*) malloc(strlen(nome) + 1);
+                    strcpy(nomes, nome);
+                } else {
+                    nomes = (char*) realloc(nomes, strlen(nomes) + strlen(nome) + 2);
+                    strcat(nomes, ",");
+                    strcat(nomes, nome);
+                }
+                printf("Nome adicionado!\n");
+                break;
+                
+            case 2:  
+                if(nomes == NULL) {
+                    printf("Lista vazia!\n");
+                    break;
+                }
+                
+                printf("Digite o nome para remover: ");
+                fgets(nome, 50, stdin);
+                nome[strlen(nome)-1] = '\0';  
+                
+                char *novaLista = NULL;
+                char *temp = strdup(nomes);
+                char *token = strtok(temp, ",");
+                int primeiro = 1;
+                
+                while(token != NULL) {
+                    if(strcmp(token, nome) != 0) {
+                        if(primeiro) {
+                            novaLista = (char*) malloc(strlen(token) + 1);
+                            strcpy(novaLista, token);
+                            primeiro = 0;
+                        } else {
+                            novaLista = (char*) realloc(novaLista, strlen(novaLista) + strlen(token) + 2);
+                            strcat(novaLista, ",");
+                            strcat(novaLista, token);
+                        }
+                    }
+                    token = strtok(NULL, ",");
+                }
+                
+                free(temp);
+                free(nomes);
+                nomes = novaLista;
+                printf("Nome removido!\n");
+                break;
+                
+            case 3:  
+                if(nomes == NULL) {
+                    printf("Lista vazia!\n");
+                } else {
+                    printf("Nomes na lista:\n");
+                    char *temp = strdup(nomes);
+                    char *token = strtok(temp, ",");
+                    int i = 1;
+                    
+                    while(token != NULL) {
+                        printf("%d. %s\n", i++, token);
+                        token = strtok(NULL, ",");
+                    }
+                    
+                    free(temp);
+                }
+                break;
+                
+            case 4:  
+                printf("Saindo...\n");
+                if(nomes != NULL) {
+                    free(nomes);
+                }
+                break;
+                
+            default:
+                printf("Opcao invalida!\n");
+        }
+        
+    } while(opcao != 4);
+    
+    return 0;
+}

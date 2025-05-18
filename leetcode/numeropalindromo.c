a6bec03404ce59daf910a23ca416058aea0f4157
DevTheusP <matheusp.estudos@gmail.com>
2025-05-18 19:35:18 -0300

    numeropalindromo

--- leetcode/numeropalindromo.c
+++ leetcode/numeropalindromo.c
@@ -1,4 +1,4 @@
-bool isPalindrome(int x) {
+/*bool isPalindrome(int x) {
     if(x < 0 || (x % 10 == 0 && x != 0)){
         return false;
     }
@@ -9,4 +9,4 @@ bool isPalindrome(int x) {
     }
     return x == metadereversa || x == metadereversa/10;
 
-}
+}*/
